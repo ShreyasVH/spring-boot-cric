a29c3bb5cd96754ecfa164e7cf57be6fb3c9f4b0
ShreyasVH <shreyas.deve.1@gmail.com>
2025-06-11 17:26:36 +0530

    removed unused import

--- src/main/java/com/springboot/cric/repositories/PlayerRepository.java
+++ src/main/java/com/springboot/cric/repositories/PlayerRepository.java
@@ -1,7 +1,6 @@
 package com.springboot.cric.repositories;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
