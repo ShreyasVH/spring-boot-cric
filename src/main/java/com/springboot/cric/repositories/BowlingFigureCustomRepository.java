0e6f712dd1a80c0fdb9424ee0b5d50993d51a6f0
ShreyasVH <shreyas.deve.1@gmail.com>
2026-04-12 02:49:45 +0530

    updates

--- src/main/java/com/springboot/cric/repositories/BowlingFigureCustomRepository.java
+++ src/main/java/com/springboot/cric/repositories/BowlingFigureCustomRepository.java
@@ -12,8 +12,8 @@ public class BowlingFigureCustomRepository extends BaseCustomRepository {
     {
         Map<String, Map<String, Integer>> statsFinal = new HashMap<>();
 
-        String query = "SELECT COUNT(*) AS innings, SUM(balls) AS balls, SUM(maidens) AS maidens, SUM(runs) AS runs, SUM(wickets) AS wickets, gt.name AS gameType, COUNT(CASE WHEN (bf.wickets >= 5 and bf.wickets < 10) then 1 end) as fifers,  COUNT(CASE WHEN (bf.wickets = 10) then 1 end) as tenWickets FROM bowling_figures bf inner join match_player_map mpm on mpm.id = bf.match_player_id and mpm.player_id = " + playerId + " INNER JOIN matches m ON m.id = mpm.match_id INNER JOIN series s ON s.id = m.series_id and m.is_official = true inner join teams t on t.id = mpm.team_id inner join team_types tt on tt.id = t.type_id and tt.name = 'International' inner join game_types gt on gt.id = s.game_type_id GROUP BY gt.name";
-        List<Map<String, Object>> result = executeRawQuery(query);
+        String query = "SELECT COUNT(*) AS innings, SUM(balls) AS balls, SUM(maidens) AS maidens, SUM(runs) AS runs, SUM(wickets) AS wickets, gt.name AS gameType, COUNT(CASE WHEN (bf.wickets >= 5 and bf.wickets < 10) then 1 end) as fifers,  COUNT(CASE WHEN (bf.wickets = 10) then 1 end) as tenWickets FROM bowling_figures bf inner join match_player_map mpm on mpm.id = bf.match_player_id and mpm.player_id = ? INNER JOIN matches m ON m.id = mpm.match_id INNER JOIN series s ON s.id = m.series_id and m.is_official = true inner join teams t on t.id = mpm.team_id inner join team_types tt on tt.id = t.type_id and tt.name = 'International' inner join game_types gt on gt.id = s.game_type_id GROUP BY gt.name";
+        List<Map<String, Object>> result = executeRawQuery(query, playerId);
 
         for(Map<String, Object> row: result)
         {
