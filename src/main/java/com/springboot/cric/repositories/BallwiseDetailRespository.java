1dc8edcc1a0f12c5f4e487a9f080c1caf611dbfb
ShreyasVH <shreyas.deve.1@gmail.com>
2026-09-15 19:06:34 +0530

    removed unused imports

--- src/main/java/com/springboot/cric/repositories/BallwiseDetailRespository.java
+++ src/main/java/com/springboot/cric/repositories/BallwiseDetailRespository.java
@@ -1,7 +1,6 @@
 package com.springboot.cric.repositories;
 
 import com.springboot.cric.models.BallwiseDetail;
-import com.springboot.cric.models.Partnership;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
